7a940fd750a444b7bc2cb4a1c5e115cfb2c35731
uc-hoba <hoba@ultracombos.com>
2016-11-11 14:38:39 +0800

    add InterfaceInfo

--- src/OSVR.cpp
+++ src/OSVR.cpp
@@ -23,26 +23,47 @@ OpenSourceVirtualReality::~OpenSourceVirtualReality()
 {
 	is_thread_running = false;
 	thd.join();
-	printf("osvr clear\n");
+	ofLogNotice(module, "clear");
 }
 
 void OpenSourceVirtualReality::threadFunction(bool serverAutoStart)
 {
 	if (serverAutoStart)
 	{
-		printf("osvrClientAttemptServerAutoStart\n");
+		ofLogNotice(module, "client attempt server auto start");
 		osvrClientAttemptServerAutoStart();
 	}
 
 	osvr::clientkit::ClientContext ctx(app_identifier.c_str(), 0);
-	osvr::clientkit::DisplayConfig display;
+	osvr::clientkit::DisplayConfig display(ctx);
 
-	//osvrRegisterPoseCallback(head.get(), directionCallback, NULL);
-	//head.registerCallback(&OpenSourceVirtualReality::poseCallback, NULL);
-	//head.registerCallback(&orientationCallback, NULL);
-	//head.registerCallback(&positionCallback, NULL);
+	// check display valid
+	if (display.valid() == false) {
+		ofLogError(module, "Could not get display config (server probably not running or not behaving), exiting.");
+		is_thread_running = false;
+	}
+	else
+	{
+		// check display startup
+		ofLogNotice(module, "display check startup");
+		float check_time_out = 5.0f;
+		float check_timestamp = ofGetElapsedTimef();
+		while (display.checkStartup() == false) {
+			ctx.update();
+			float dt = ofGetElapsedTimef() - check_timestamp;
+			if (dt > check_time_out)
+			{
+				is_thread_running = false;
+				ofLogWarning(module, "display check time out");
+				break;
+			}
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}
+		if (is_thread_running)
+			ofLogNotice(module, "display startup status is good");
+	}
 
-	//printf("osvr while loop\n");
+
 	while (is_thread_running)
 	{
 		{
@@ -51,8 +72,12 @@ void OpenSourceVirtualReality::threadFunction(bool serverAutoStart)
 			{
 				for (auto& path : interface_paths)
 				{
-					printf("add interface: %s\n", path.c_str());
-					interfaces[path] = ctx.getInterface(path);
+					ofLogNotice(module, "interface add: %s\n", path.c_str());
+
+					interfaces[path] = InterfaceInfo();
+					InterfaceInfo& info = interfaces[path];
+					info.interface = ctx.getInterface(path);
+					//info.interface.registerCallback((OSVR_PoseCallback)OpenSourceVirtualReality::poseCallback, this);
 				}
 				interface_paths.clear();
 			}
@@ -60,111 +85,126 @@ void OpenSourceVirtualReality::threadFunction(bool serverAutoStart)
 
 		ctx.update();
 
-		// display
-		display = osvr::clientkit::DisplayConfig(ctx);
-
-		if (display.valid())
+		printf("display valid\n");
+		printf("%u viewers\n", display.getNumViewers());
+		int num_eye = 0;
+		display.forEachEye([&](osvr::clientkit::Eye eye)
 		{
-			display.forEachEye([](osvr::clientkit::Eye eye)
+			num_eye++;
+			int num_suf = 0;
+			eye.forEachSurface([&](osvr::clientkit::Surface surface)
 			{
-				double view_mat[OSVR_MATRIX_SIZE];
-				eye.getViewMatrix(OSVR_MATRIX_COLMAJOR | OSVR_MATRIX_COLVECTORS, view_mat);
+				num_suf++;
+			});
+			printf("%u surfaces\n", num_suf);
+		});
+		printf("%u eye\n", num_eye);
+#if 0
+		display.forEachEye([&](osvr::clientkit::Eye eye)
+		{
+			float view_mat[OSVR_MATRIX_SIZE];
+			if (eye.getViewMatrix(OSVR_MATRIX_COLMAJOR | OSVR_MATRIX_COLVECTORS, view_mat))
+			{
 				// apply model view matrix
+				//model_view_matrix;
 
+				memcpy(model_view_matrix.getPtr(), view_mat, sizeof(float) * OSVR_MATRIX_SIZE);
+			}
 
-				eye.forEachSurface([](osvr::clientkit::Surface surface)
-				{
-					auto viewport = surface.getRelativeViewport();
-					// apply viewport
+			eye.forEachSurface([](osvr::clientkit::Surface surface)
+			{
+				auto viewport = surface.getRelativeViewport();
+				// apply viewport
 
-					double z_near = 0.1;
-					double z_far = 100;
-					double proj_mat[OSVR_MATRIX_SIZE];
-					surface.getProjectionMatrix(z_near, z_far, OSVR_MATRIX_COLMAJOR | OSVR_MATRIX_COLVECTORS | OSVR_MATRIX_SIGNEDZ | OSVR_MATRIX_RHINPUT, proj_mat);
-					// apply projection matrix
+				float z_near = 0.1;
+				float z_far = 100;
+				float proj_mat[OSVR_MATRIX_SIZE];
+				surface.getProjectionMatrix(z_near, z_far, OSVR_MATRIX_COLMAJOR | OSVR_MATRIX_COLVECTORS | OSVR_MATRIX_SIGNEDZ | OSVR_MATRIX_RHINPUT, proj_mat);
+				// apply projection matrix
 
+			});
+		});
+
+		for (uint32_t i = 0; i < display.getNumViewers(); i++)
+		{
+			osvr::clientkit::Viewer viewer = display.getViewer(i);
+			std::cout << "Viewer " << viewer.getViewerID() << "\n";
+
+			viewer.forEachEye([](osvr::clientkit::Eye eye) {
+				std::cout << "\tEye " << int(eye.getEyeID()) << "\n";
+				eye.forEachSurface([](osvr::clientkit::Surface surface) {
+					std::cout << "\t\tSurface " << surface.getSurfaceID() << "\n";
 				});
 			});
-
-			for (uint32_t i = 0; i < display.getNumViewers(); i++)
-			{
-				osvr::clientkit::Viewer viewer = display.getViewer(i);
-				std::cout << "Viewer " << viewer.getViewerID() << "\n";
-
-				viewer.forEachEye([](osvr::clientkit::Eye eye) {
-					std::cout << "\tEye " << int(eye.getEyeID()) << "\n";
-					eye.forEachSurface([](osvr::clientkit::Surface surface) {
-						std::cout << "\t\tSurface " << surface.getSurfaceID() << "\n";
-					});
-				});
-			}
 		}
-
+#endif
 
 		// interface state
 		for (auto& f : interfaces)
 		{
-			osvr::clientkit::Interface curr_interface = f.second;
 			printf("check for interface: %s\n", f.first.c_str());
-			OSVR_PoseState state;
-			OSVR_TimeValue timestamp;
-			OSVR_ReturnCode ret = osvrGetPoseState(curr_interface.get(), &timestamp, &state);
+			InterfaceInfo& info = f.second;
+			OSVR_ReturnCode ret = osvrGetPoseState(info.interface.get(), &info.timestamp, &info.state);
 			if (ret != OSVR_RETURN_SUCCESS) {
 				printf("No pose state!\n");
 			}
 			else {
-				printf("Got POSE state: Position = (%f, %f, %f), orientation = (%f, %f, %f, %f)\n",
-					state.translation.data[0], 
-					state.translation.data[1],
-					state.translation.data[2],
-					osvrQuatGetW(&(state.rotation)),
-					osvrQuatGetX(&(state.rotation)),
-					osvrQuatGetY(&(state.rotation)),
-					osvrQuatGetZ(&(state.rotation)));
+				std::lock_guard<std::mutex> guard(mtx);
 			}
 		}
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000 / fps));
 	}
+
+	ofLogNotice(module, "thread exit");
 }
+
+void OpenSourceVirtualReality::poseCallback(void *userdata, const OSVR_TimeValue *timestamp, const OSVR_PoseReport *report)
+{
+	printf("Got POSE report: Position = (%f, %f, %f), orientation = (%f, %f, %f, %f)\n",
+		report->pose.translation.data[0], 
+		report->pose.translation.data[1],
+		report->pose.translation.data[2],
+		osvrQuatGetW(&(report->pose.rotation)),
+		osvrQuatGetX(&(report->pose.rotation)),
+		osvrQuatGetY(&(report->pose.rotation)),
+		osvrQuatGetZ(&(report->pose.rotation)));
+
+	OpenSourceVirtualReality* ptr = (OpenSourceVirtualReality*)userdata;
+
+}
+
+void OpenSourceVirtualReality::orientationCallback(void *userdata, const OSVR_TimeValue *timestamp, const OSVR_OrientationReport *report)
+{
+	printf("Got ORIENTATION report: Orientation = (%f, %f, %f, %f)\n",
+		osvrQuatGetW(&(report->rotation)), 
+		osvrQuatGetX(&(report->rotation)),
+		osvrQuatGetY(&(report->rotation)),
+		osvrQuatGetZ(&(report->rotation)));
+
+	OpenSourceVirtualReality* ptr = (OpenSourceVirtualReality*)userdata;
+}
+
+void OpenSourceVirtualReality::positionCallback(void *userdata, const OSVR_TimeValue *timestamp, const OSVR_PositionReport *report)
+{
+	printf("Got POSITION report: Position = (%f, %f, %f)\n",
+		report->xyz.data[0], report->xyz.data[1], report->xyz.data[2]);
+
+	OpenSourceVirtualReality* ptr = (OpenSourceVirtualReality*)userdata;
+}
+
+
 
 // only for callback function template
 namespace
 {
-	void poseCallback(void *userdata, const OSVR_TimeValue *timestamp,
-		const OSVR_PoseReport *report) {
-		printf("Got POSE report: Position = (%f, %f, %f), orientation = (%f, %f, %f, %f)\n",
-			report->pose.translation.data[0], report->pose.translation.data[1],
-			report->pose.translation.data[2],
-			osvrQuatGetW(&(report->pose.rotation)),
-			osvrQuatGetX(&(report->pose.rotation)),
-			osvrQuatGetY(&(report->pose.rotation)),
-			osvrQuatGetZ(&(report->pose.rotation)));
-	}
-
-	void orientationCallback(void *userdata, const OSVR_TimeValue *timestamp,
-		const OSVR_OrientationReport *report) {
-		printf("Got ORIENTATION report: Orientation = (%f, %f, %f, %f)\n",
-			osvrQuatGetW(&(report->rotation)), osvrQuatGetX(&(report->rotation)),
-			osvrQuatGetY(&(report->rotation)),
-			osvrQuatGetZ(&(report->rotation)));
-	}
-
-	void positionCallback(void *userdata, const OSVR_TimeValue *timestamp,
-		const OSVR_PositionReport *report) {
-		printf("Got POSITION report: Position = (%f, %f, %f)\n",
-			report->xyz.data[0], report->xyz.data[1], report->xyz.data[2]);
-	}
-
 	void printDirectionReport(const OSVR_DirectionReport *report) {
 
 		std::cout << report->direction.data[0] << "; " << report->direction.data[1]
 			<< "; " << report->direction.data[2] << "\t" << std::endl;
 	}
 
-	void directionCallback(void * /*userdata*/,
-		const OSVR_TimeValue * /*timestamp*/,
-		const OSVR_DirectionReport *report) {
+	void directionCallback(void * /*userdata*/, const OSVR_TimeValue * /*timestamp*/, const OSVR_DirectionReport *report) {
 		std::cout << "Got 3D direction Report, for sensor #" << report->sensor
 			<< std::endl;
 		printDirectionReport(report);
--- src/OSVR.h
+++ src/OSVR.h
@@ -29,18 +29,65 @@ public:
 		interface_paths.push_back(path);
 	}
 
+	bool getInterfacePose(const std::string& path, ofVec3f& translation, ofQuaternion& rotation)
+	{
+		std::lock_guard<std::mutex> guard(mtx);
+		if (interfaces.find(path) == interfaces.end())
+		{
+			ofLogWarning(module, "interface is not found with path: %s", path.c_str());
+			return false;
+		}
+			
+
+		auto& state = interfaces[path].state;
+		double x, y, z, w;
+
+		x = state.translation.data[0];
+		y = state.translation.data[1];
+		z = state.translation.data[2];
+		translation.set(x, y, z);
+
+		x = osvrQuatGetX(&(state.rotation));
+		y = osvrQuatGetY(&(state.rotation));
+		z = osvrQuatGetZ(&(state.rotation));
+		w = osvrQuatGetW(&(state.rotation));
+		rotation.set(x, y, z, w);
+
+		return true;
+	}
+
+protected:
+	struct InterfaceInfo
+	{	
+		osvr::clientkit::Interface interface;
+		ofVec3f translation;
+		
+		OSVR_PoseState state;
+		OSVR_TimeValue timestamp;
+	};
+
+
 private:
 	OpenSourceVirtualReality(std::string applicationIdentifier, bool serverAutoStart);
-
+	
 	void threadFunction(bool serverAutoStart);
+
+	static void poseCallback(void *userdata, const OSVR_TimeValue *timestamp, const OSVR_PoseReport *report);
+	static void orientationCallback(void *userdata, const OSVR_TimeValue *timestamp, const OSVR_OrientationReport *report);
+	static void positionCallback(void *userdata, const OSVR_TimeValue *timestamp, const OSVR_PositionReport *report);
 
 	std::thread thd;
 	std::mutex mtx;
 	std::string app_identifier = "";
 	bool is_thread_running = true;
 	int fps = 60;
+	const string module = "OSVR";
+
+	ofMatrix4x4 modelview_matrix;
+	ofRectangle viewport;
+	ofMatrix4x4 projection_matrix;
 
 	std::deque<std::string> interface_paths;
-	std::map<std::string, osvr::clientkit::Interface> interfaces;
-
+	//std::map<std::string, osvr::clientkit::Interface> interfaces;
+	std::map<std::string, InterfaceInfo> interfaces;
 };
